58a6d78c246aa7326994344b1553f1fff5051e7d
Peter Hoek <peter@hoek.io>
2025-08-31 19:55:54 +1000

    final version? keel hehe

--- main/main.c
+++ main/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <math.h>
+#include <string.h>
 #include "sdkconfig.h"
 #include "esp_log.h"
 #include "esp_timer.h"
@@ -24,28 +25,39 @@ static const char *TAG = "energymon";
 #define MOSI_PIN GPIO_NUM_19 // GPIO19 - MOSI (unused)
 
 #define BLUE_LED GPIO_NUM_32 // GPIO32 - Blue LED
-#define RED_LED GPIO_NUM_13 // GPIO? - Red LED
-// #define GREEN_LED GPIO_NUM_13 // GPIO? - Green LED
+#define RED_LED GPIO_NUM_13 // GPIO13 - Red LED
+#define GREEN_LED GPIO_NUM_14 // GPIO14 - Green LED
 
 #define NUM_SAMPLES 2000
 
-#define MAINS_CURRENT_CH 0
-#define VOLTAGE_CH 1
-
 static int32_t adcOffset[8]; // offsets for calibration
+static bool dcMode; // set true for DC testing, false for AC
+static int mainsCurrentCh; // (unimportant) used for blue led net power out/in status
+static int mainsVoltageCh; //   (important) used for calculating period of mains to sync periods
+                           //               NOTE: we want to be using the voltage for this instead of
+                           //               (one of) the current channels, because the voltage is always
+                           //               much greater than zero (and the current could sometimes be small).
 
+static SemaphoreHandle_t config_param_lock;
+static StaticSemaphore_t config_param_lock_buff;
+
+#define INIT_PARAM_OFFSET  (1 << 0)
+#define INIT_PARAM_DC_MODE (1 << 1)
+#define INIT_PARAM_MAINS_CURRENT_CH (1 << 2)
+#define INIT_PARAM_MAINS_VOLTAGE_CH (1 << 3)
+#define INIT_PARAM_ALL (INIT_PARAM_OFFSET | INIT_PARAM_DC_MODE | INIT_PARAM_MAINS_CURRENT_CH | INIT_PARAM_MAINS_VOLTAGE_CH)
+
+static EventGroupHandle_t config_param_is_init;
+static StaticEventGroup_t config_param_is_init_buff;
+
+#define ERROR_LED_TIMEOUT_US (5 * 1000 * 1000)
+
+static int64_t lastErrorTimeUs = -ERROR_LED_TIMEOUT_US;
 static uint8_t rawBytes[18];
 static int32_t adcData[NUM_SAMPLES][8];
 static double calcAvg[8];
 static double calcRms[8];
 static double calcPwr[8];
-
-#define INIT_PARAM_OFFSET  (1 << 0)
-#define INIT_PARAM_DC_MODE (1 << 1)
-#define INIT_PARAM_DC_MODE_ENABLED (1 << 2) // set true for DC testing, false for AC
-
-static EventGroupHandle_t init_params;
-static StaticEventGroup_t init_params_buff;
 
 static void calcRmsAndPower(int ch, int startSample, int endSample)
 {
@@ -57,7 +69,7 @@ static void calcRmsAndPower(int ch, int startSample, int endSample)
         int32_t val = adcData[i][ch];
         sum += val;
         sumRms += val * val;
-        sumPwr += val * adcData[i][VOLTAGE_CH];
+        sumPwr += val * adcData[i][mainsVoltageCh];
     }
 
     uint32_t numSamples = endSample - startSample;
@@ -136,7 +148,7 @@ static void readAllChannels(spi_device_handle_t spi, int32_t *data)
 
 static bool findEndpoints(int *outStartSample, int *outEndSample)
 {
-    if (xEventGroupGetBits(init_params) & INIT_PARAM_DC_MODE_ENABLED)
+    if (dcMode)
     {
         *outStartSample = 0;
         *outEndSample = NUM_SAMPLES - 1;
@@ -145,10 +157,10 @@ static bool findEndpoints(int *outStartSample, int *outEndSample)
 
     int startSample = 1;
 
-    bool sign = adcData[0][VOLTAGE_CH] >= 0;
+    bool sign = adcData[0][mainsVoltageCh] >= 0;
 
     //  Advance `startSample` to the first sample which differs in sign from the literal zeroth sample (this has to be a zero crossing)
-    while (startSample < NUM_SAMPLES && ((adcData[startSample][VOLTAGE_CH] >= 0) == sign))
+    while (startSample < NUM_SAMPLES && ((adcData[startSample][mainsVoltageCh] >= 0) == sign))
     {
         startSample++;
     }
@@ -161,41 +173,39 @@ static bool findEndpoints(int *outStartSample, int *outEndSample)
 
     int halfPeriods = 0;
     int endSample = startSample;
-    bool endSign = adcData[endSample][VOLTAGE_CH] >= 0;
+    bool endSign = adcData[endSample][mainsVoltageCh] >= 0;
 
     // Scan over all of the samples after `startSample` until the end
     for (int currentSample = startSample + 1; currentSample < NUM_SAMPLES; currentSample++)
     {
-        bool currentSign = adcData[currentSample][VOLTAGE_CH] >= 0;
+        bool currentSign = adcData[currentSample][mainsVoltageCh] >= 0;
 
         // If the current sample now has a different sign to the last saved "end sample", updated
         // the saved "end sample" to this position (we have just found another zero crossing).
         if (currentSign != endSign)
         {
+            // Have we see roughly the expected number of half periods?
+            // NOTE: 136 samples/halfperiod is a reasonable estimate for x16 oversampling, and
+            // should be suitably multiplied/divided if the oversampling rate is changed.
+            #define EXPECTED_SAMPLES_PER_HALFPERIOD 136
+
+            if (abs(EXPECTED_SAMPLES_PER_HALFPERIOD - (currentSample - endSample)) > EXPECTED_SAMPLES_PER_HALFPERIOD * 0.03)
+            {
+                printf("half period has bad length: %d vs %d - %d = %d\n",
+                    EXPECTED_SAMPLES_PER_HALFPERIOD,
+                    currentSample, endSample, currentSample - endSample);
+                return false;
+            }
+
             endSample = currentSample;
             endSign = currentSign;
             halfPeriods++;
         }
     }
 
-    printf("%d\n", halfPeriods);
-
     if (!halfPeriods)
     {
         printf("didn't find any half periods!?!\n");
-        return false;
-    }
-
-    // Have we see roughly the expected number of half periods?
-    // NOTE: 138 samples/halfperiod is a reasonable estimate for x16 oversampling, and
-    // should be suitably multiplied/divided if the oversampling rate is changed.
-#define EXPECTED_SAMPLES_PER_HALFPERIOD 136
-
-    if (abs((halfPeriods * EXPECTED_SAMPLES_PER_HALFPERIOD) - (endSample - startSample)) > NUM_SAMPLES / 200)
-    {
-        printf("half periods and range disagree! halfPeriods=%d: %d vs %d - %d = %d\n", halfPeriods,
-               halfPeriods * EXPECTED_SAMPLES_PER_HALFPERIOD,
-               endSample, startSample, endSample - startSample);
         return false;
     }
 
@@ -205,18 +215,19 @@ static bool findEndpoints(int *outStartSample, int *outEndSample)
 }
 
 static void setLedStatus() {
-    gpio_set_level(BLUE_LED, calcPwr[MAINS_CURRENT_CH] < -0.5);
-    gpio_set_level( RED_LED, dadlib_is_connected());
+    gpio_set_level( BLUE_LED, calcPwr[mainsCurrentCh] < -0.5);
+    gpio_set_level(  RED_LED, esp_timer_get_time() - lastErrorTimeUs < ERROR_LED_TIMEOUT_US);
+    gpio_set_level(GREEN_LED, dadlib_is_connected());
 }
 
 static void publishResults()
 {
     char buf[512];
-    int ret = snprintf(buf, sizeof(buf), "{\"dcMode\":%s,\"primaryCh\":%d,"
+    int ret = snprintf(buf, sizeof(buf), "{\"dcMode\":%s,\"mainsVoltageCh\":%d,"
                                          "\"avg\":[%.6lf,%.6lf,%.6lf,%.6lf,%.6lf,%.6lf,%.6lf,%.6lf],"
                                          "\"rms\":[%.6lf,%.6lf,%.6lf,%.6lf,%.6lf,%.6lf,%.6lf,%.6lf],"
                                          "\"pwr\":[%.6lf,%.6lf,%.6lf,%.6lf,%.6lf,%.6lf,%.6lf,%.6lf]}",
-                       (xEventGroupGetBits(init_params) & INIT_PARAM_DC_MODE_ENABLED) ? "true" : "false", VOLTAGE_CH,
+                       dcMode ? "true" : "false", mainsVoltageCh,
                        calcAvg[0], calcAvg[1], calcAvg[2], calcAvg[3], calcAvg[4], calcAvg[5], calcAvg[6], calcAvg[7],
                        calcRms[0], calcRms[1], calcRms[2], calcRms[3], calcRms[4], calcRms[5], calcRms[6], calcRms[7],
                        calcPwr[0], calcPwr[1], calcPwr[2], calcPwr[3], calcPwr[4], calcPwr[5], calcPwr[6], calcPwr[7]);
@@ -226,23 +237,31 @@ static void publishResults()
         dadlib_panic("MQTT JSON buffer too small");
     }
 
-    if (dadlib_mqtt_publish(MQTT_OUT_TOPIC, buf, 0, 1) < 0)
+    if (dadlib_mqtt_publish(MQTT_OUT_TOPIC, buf, 0, 0) < 0)
     {
         ESP_LOGW(TAG, "MQTT publish failed");
         printf("%s\n", buf); // Print JSON to terminal every time
     }
 }
 
+#define SCRATCH_BUFF_SIZE 32
+
 static void handle_mqtt_set_offset(const char *data, int len) {
     printf("received offset data: %.*s\n", len, data);
 
-    if (xEventGroupGetBits(init_params) & INIT_PARAM_OFFSET) {
-        dadlib_panic("offsets were already initialized, restarting");
+    if (len >= SCRATCH_BUFF_SIZE - 1) {
+        dadlib_panic("data too long!");
     }
+
+    char buff[SCRATCH_BUFF_SIZE];
+    memcpy(buff, data, len);
+    buff[len] = 0;
 
     printf("setting offsets to: ");
 
-    const char *cur = data;
+    int32_t newOffsets[8];
+
+    const char *cur = buff;
     for (int i = 0; i < 8; i++) {
         char *end;
         long val = strtol(cur, &end, 10);
@@ -251,8 +270,8 @@ static void handle_mqtt_set_offset(const char *data, int len) {
             dadlib_panic("offset fail");
         }
 
-        adcOffset[i] = (int32_t) val;
-        printf("%ld, ", adcOffset[i]);
+        newOffsets[i] = (int32_t) val;
+        printf("%ld, ", newOffsets[i]);
 
         cur = end;
         if (*cur != ',') {
@@ -264,7 +283,11 @@ static void handle_mqtt_set_offset(const char *data, int len) {
 
     printf("\n");
 
-    xEventGroupSetBits(init_params, INIT_PARAM_OFFSET);
+    xSemaphoreTake(config_param_lock, portMAX_DELAY);
+    memcpy(adcOffset, newOffsets, sizeof(adcOffset));
+    xSemaphoreGive(config_param_lock);
+
+    xEventGroupSetBits(config_param_is_init, INIT_PARAM_OFFSET);
 }
 
 static void handle_mqtt_set_dcMode(const char *data, int len) {
@@ -274,29 +297,85 @@ static void handle_mqtt_set_dcMode(const char *data, int len) {
         dadlib_panic("dcMode data too long");
     }
 
+    bool newDcMode;
     if (data[0] == '0') {
-        xEventGroupClearBits(init_params, INIT_PARAM_DC_MODE_ENABLED);
+        newDcMode = false;
     } else if (data[0] == '1') {
-        xEventGroupSetBits(init_params, INIT_PARAM_DC_MODE_ENABLED);
+        newDcMode = true;
     } else {
         dadlib_panic("invalid dcMode data");
     }
 
-    xEventGroupSetBits(init_params, INIT_PARAM_DC_MODE);
+    printf("setting dcMode to: %d\n", newDcMode);
+
+    xSemaphoreTake(config_param_lock, portMAX_DELAY);
+    dcMode = newDcMode;
+    xSemaphoreGive(config_param_lock);
+
+    xEventGroupSetBits(config_param_is_init, INIT_PARAM_DC_MODE);
+}
+
+static void handle_mqtt_set_mainsCurrentCh(const char *data, int len) {
+    printf("received mainsCurrentCh data: %.*s\n", len, data);
+
+    if (len >= SCRATCH_BUFF_SIZE - 1) {
+        dadlib_panic("data too long!");
+    }
+
+    char buff[SCRATCH_BUFF_SIZE];
+    memcpy(buff, data, len);
+    buff[len] = 0;
+
+    long newCurrentCh = strtol(buff, NULL, 10);
+
+    printf("setting mainsCurrentCh to: %ld\n", newCurrentCh);
+
+    xSemaphoreTake(config_param_lock, portMAX_DELAY);
+    mainsCurrentCh = newCurrentCh;
+    xSemaphoreGive(config_param_lock);
+
+    xEventGroupSetBits(config_param_is_init, INIT_PARAM_MAINS_CURRENT_CH);
+}
+
+static void handle_mqtt_set_mainsVoltageCh(const char *data, int len) {
+    printf("received mainsVoltageCh data: %.*s\n", len, data);
+
+    if (len >= SCRATCH_BUFF_SIZE - 1) {
+        dadlib_panic("data too long!");
+    }
+
+    char buff[SCRATCH_BUFF_SIZE];
+    memcpy(buff, data, len);
+    buff[len] = 0;
+
+    long newVoltageCh = strtol(buff, NULL, 10);
+
+    printf("setting mainsVoltageCh to: %ld\n", newVoltageCh);
+
+    xSemaphoreTake(config_param_lock, portMAX_DELAY);
+    mainsVoltageCh = newVoltageCh;
+    xSemaphoreGive(config_param_lock);
+
+    xEventGroupSetBits(config_param_is_init, INIT_PARAM_MAINS_VOLTAGE_CH);
 }
 
 static void loop(spi_device_handle_t spi)
 {
+    // Step 1: read NUM_SAMPLES samples from the ADC (safe to do without holding the config params lock as they are not used here)
     doRead(spi);
     for (int c = 0; c < NUM_SAMPLES; c++)
     {
         readAllChannels(spi, adcData[c]);
     }
 
+    // Step 2: calculate RMS and power for each channel, which uses the config params and so needs to hold the lock
+    xSemaphoreTake(config_param_lock, portMAX_DELAY);
+
     int startSample, endSample;
     if (!findEndpoints(&startSample, &endSample))
     {
-        return;
+        lastErrorTimeUs = esp_timer_get_time();
+        goto done;
     }
 
     for (int ch = 0; ch < 8; ch++)
@@ -304,17 +383,24 @@ static void loop(spi_device_handle_t spi)
         calcRmsAndPower(ch, startSample, endSample);
     }
 
+    publishResults(); // Send to MQTT, uses the config params
+
+    done:
     setLedStatus();
-    publishResults(); // Send to MQTT
+    xSemaphoreGive(config_param_lock);
 }
 
 void app_main()
 {
     printf("Starting energy monitor...\n");
     
-    init_params = xEventGroupCreateStatic(&init_params_buff);
+    config_param_lock = xSemaphoreCreateBinaryStatic(&config_param_lock_buff);
+    config_param_is_init = xEventGroupCreateStatic(&config_param_is_init_buff);
 
-    static dadlib_mqtt_topic_handler_t mqtt_subscribe_topics[] = {
+    // Initially the lock is available
+    xSemaphoreGive(config_param_lock);
+
+    dadlib_mqtt_topic_handler_t mqtt_subscribe_topics[] = {
         {
             .topic = "esp32/energymon/offset",
             .handler = handle_mqtt_set_offset,
@@ -322,6 +408,14 @@ void app_main()
         {
             .topic = "esp32/energymon/dcMode",
             .handler = handle_mqtt_set_dcMode,
+        },
+        {
+            .topic = "esp32/energymon/mainsCurrentCh",
+            .handler = handle_mqtt_set_mainsCurrentCh,
+        },
+        {
+            .topic = "esp32/energymon/mainsVoltageCh",
+            .handler = handle_mqtt_set_mainsVoltageCh,
         },
         { NULL }
     };
@@ -371,13 +465,13 @@ void app_main()
     spi_device_handle_t spi;
     ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &spi));
 
-    vTaskSuspend
-
-    // wait until all adc offsets are initialized
-    EventBits_t ret = xEventGroupWaitBits(init_params, INIT_PARAM_OFFSET | INIT_PARAM_DC_MODE, pdFALSE, pdTRUE, pdMS_TO_TICKS(5000));
-    if (!(ret & INIT_PARAM_OFFSET) && !(ret & INIT_PARAM_DC_MODE)) {
+    // wait until all config params are initialized
+    EventBits_t ret = xEventGroupWaitBits(config_param_is_init, INIT_PARAM_ALL, pdFALSE, pdTRUE, pdMS_TO_TICKS(5000));
+    if (ret != INIT_PARAM_ALL) {
         dadlib_panic("took too long to get init params");
     }
+
+    vTaskPrioritySet(NULL, 20);
 
     while (true)
     {
